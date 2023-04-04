2b9b21857083d2ea9f4a498d9ea7d8db7d8449c4
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-04-05 00:46:05 +0200

    requisito 8

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,16 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(1, 'computador', 'tekcompany',
+                      '2023-03-25', '2027-03-25', "5497", "guardar na caixa")
+
+    relatorio = (
+        f"O produto {produto.nome_do_produto}"
+        f" fabricado em {produto.data_de_fabricacao}"
+        f" por {produto.nome_da_empresa} com validade"
+        f" até {produto.data_de_validade}"
+        f" precisa ser armazenado {produto.instrucoes_de_armazenamento}."
+    )
+
+    assert repr(produto) == relatorio
