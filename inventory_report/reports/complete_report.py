7322471e0c1dd9b4ae6ec883a43d30e8151119f7
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-04-04 22:08:43 +0200

    tentando exercicio 3 com ajuda

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,24 @@
-class CompleteReport:
-    pass
+from collections import Counter
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls,  produtos: 'list[dict]'):
+        relatorio_simples = super().generate(produtos)
+
+        empresa = [
+            produto['nome_da_empresa']
+            for produto in produtos
+        ]
+
+        quantidade_por_empresa = Counter(empresa)
+        relatorio = ''
+        for empresa, quantidade in quantidade_por_empresa.items():
+            relatorio += f'- {empresa}: {quantidade}\n'
+
+        return (
+            f'{relatorio_simples}'
+            f'\nProdutos estocados por empresa:\n'
+            f'{relatorio}'
+        )
