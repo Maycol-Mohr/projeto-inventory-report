abc20c7488cc4f42c493413f4af520b426878a61
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-04-04 20:08:01 +0200

    requisito 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,13 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui teste
+    produto = Product(1, 'computador', 'tekcompany',
+                      '2023-03-25', '2027-03-25', "5497", "guardar na caixa")
+    assert produto.id == 1
+    assert produto.nome_do_produto == 'computador'
+    assert produto.nome_da_empresa == 'tekcompany'
+    assert produto.data_de_fabricacao == '2023-03-25'
+    assert produto.data_de_validade == '2027-03-25'
+    assert produto.numero_de_serie == '5497'
+    assert produto.instrucoes_de_armazenamento == 'guardar na caixa'
