54eb7cb9e36c04bc8f14c088b70ccf045d649b6e
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-04-04 23:28:32 +0200

    tentando exercicio 5 com ajuda

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,22 +1,40 @@
-import csv
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
+import csv
+import json
+# import xml.etree.ElementTree as ET
 
 
 class Inventory:
     @classmethod
     def import_data(cls, path: str, tipo_relatorio: str):
-        with open(path, encoding='utf-8') as file:
-            arquivo_prod = csv.DictReader(file, delimiter=',', quotechar='"')
-            ler_produtos = []
-            for produto in arquivo_prod:
-                ler_produtos.append(produto)
-            ler_produtos = list(ler_produtos)
 
+        lista_produtos = cls.ler_arquivo(path)
         if tipo_relatorio == 'simples':
-            return SimpleReport.generate(ler_produtos)
+            return SimpleReport.generate(lista_produtos)
         elif tipo_relatorio == 'completo':
-            return CompleteReport.generate(ler_produtos)
+            return CompleteReport.generate(lista_produtos)
         raise ValueError(
             'Erro ao ler o arquivo de relatório'
         )
+
+    @classmethod
+    def ler_arquivo(cls, path: str):
+        if path.endswith('csv'):
+            with open(path, encoding='utf-8') as file:
+                aquivo_csv = csv.DictReader(file, delimiter=',', quotechar='"')
+                ler_produtos = []
+                for produto in aquivo_csv:
+                    ler_produtos.append(produto)
+                ler_produtos = list(ler_produtos)
+                return ler_produtos
+
+        elif path.endswith('json'):
+            with open(path) as file:
+                content = file.read()
+                ler_relatorio = json.loads(content)
+                return ler_relatorio
+
+        # with open(path) as file:
+        #     tree = ET.parse(file)  # leitura do arquivo
+        #     root = tree.getroot()
