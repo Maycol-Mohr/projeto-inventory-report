040dc20cba4608bd299019492135d0d2331ee4fa
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-04-04 21:21:05 +0200

    tentando exercicio 2 com ajuda

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,34 @@
+from datetime import date
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @classmethod
+    def generate(cls,  produtos: 'list[dict]'):
+        data_fabricacao = [
+            produto['data_de_fabricacao']
+            for produto in produtos
+        ]
+
+        data_de_validade = [
+            produto['data_de_validade']
+            for produto in produtos
+        ]
+
+        emprsa = [
+            produto['nome_da_empresa']
+            for produto in produtos
+        ]
+
+        data_mais_antiga = min(data_fabricacao)
+        data_mais_proxima = str(date.max)
+
+        for data in data_de_validade:
+            if data <= data_mais_proxima and data >= str(date.today()):
+                data_mais_proxima = data
+
+        return (
+            f'Data de fabricação mais antiga: {data_mais_antiga}\n'
+            f'Data de validade mais próxima: {data_mais_proxima}\n'
+            f'Empresa com mais produtos: {Counter(emprsa).most_common()[0][0]}'
+        )
